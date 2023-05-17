557e8e187bb520027b6f6825961aae00fb855a7a
ghifarit53 <mghifarit53+github@gmail.com>
2023-05-17 21:13:31 +0700

    add comment field

--- src/main/java/com/pweb/reddits/repo/CommentRepo.java
+++ src/main/java/com/pweb/reddits/repo/CommentRepo.java
@@ -1,9 +1,9 @@
 package com.pweb.reddits.repo;
 
-import com.pweb.reddits.entity.User;
+import com.pweb.reddits.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface CommentRepo extends JpaRepository<Comment, Long> {
 }
